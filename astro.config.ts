3f33a6d3f3ef18151a5f9d111ed05f729687f451
Frank Fava <frank.fava@gmail.com>
2026-08-23 00:10:57 +0200

    build: install alpinejs

--- astro.config.ts
+++ astro.config.ts
@@ -2,14 +2,23 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import netlify from "@astrojs/netlify";
+import alpine from "@astrojs/alpinejs";
 
 const isDev = process.env.NODE_ENV == "development";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: isDev ? undefined : netlify(),
+	adapter: isDev ? undefined : netlify(),
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				alpinejs: "@alpinejs/csp",
+			},
+		},
+	},
+	integrations: [
+		alpine({ entrypoint: "/src/alpine/entrypoint.js" }),
+	]
 });
