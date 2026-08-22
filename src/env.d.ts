899eb8404b85394ccd89f2c2020f81fcbcdd841b
Frank Fava <frank.fava@gmail.com>
2026-08-23 00:17:19 +0200

    chore: env type def

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
+
 interface Window {
 	Alpine: import("alpinejs").Alpine;
 }
