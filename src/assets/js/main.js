7cc3fee97a6e685d7c10a1be30e3e9382d99e696
Frank Fava <frank.fava@gmail.com>
2026-08-28 02:11:54 +0200

    feat: main JS

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,31 @@
-//
+function initReducedMotion() {
+	if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+		document.documentElement.classList.add("motion-safe:scroll-smooth");
+	}
+	window.matchMedia("(prefers-reduced-motion: reduce)").addEventListener("change", (e) => {
+		document.documentElement.classList.toggle("motion-safe:scroll-smooth", !e.matches);
+	});
+}
+
+/**
+ * Init
+ */
+const setup = () => {
+	initReducedMotion();
+};
+
+const refreshOnResize = () => {
+	//
+};
+
+const loadIt = () => {
+	setup();
+
+	let timeout;
+	window.addEventListener("resize", () => {
+		clearTimeout(timeout);
+		timeout = setTimeout(refreshOnResize, 100);
+	});
+};
+
+loadIt();
